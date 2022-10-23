eb04a9c09c40e5e9d414bea84c8719fef912c4b3
kairox <git@oxymoron.link>
2022-10-23 19:28:23 +0900

    create runtime

---
+++ src/Main.cs
@@ -0,0 +1,32 @@
+﻿using Newtonsoft.Json.Linq;
+
+class SampleLambda : Serverless.Lambda
+{
+  static async Task Main(string[] args)
+  {
+    await Handler("hello", _ =>
+    {
+      return new JObject
+      {
+        ["statusCode"] = 200,
+        ["body"] = new JObject
+        {
+          ["msg"] = "さよなら透明だった僕たち",
+        }.ToString(),
+      };
+    });
+
+    await Handler("world", input =>
+    {
+      return new JObject
+      {
+        ["statusCode"] = 200,
+        ["body"] = new JObject
+        {
+          ["msg"] = "でしょうねミスター・サーバーレス",
+          ["event"] = input.GetValue("body"),
+        }.ToString(),
+      };
+    });
+  }
+}
